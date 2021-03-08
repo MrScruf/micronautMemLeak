48ec55a8d408ef25c5c9c54e4b32fc0dbc58391f
Zdeněk Krupička <krupizde@fit.cvut.cz>
2021-03-08 09:07:49 +0100

    initial commit

---
+++ src/main/java/net/krupizde/TestController.java
@@ -0,0 +1,29 @@
+package net.krupizde;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Consumes;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.types.files.FileCustomizableResponseType;
+import io.micronaut.http.server.types.files.StreamedFile;
+
+import javax.inject.Inject;
+import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Controller
+public class TestController {
+    @Inject
+    XmlService xmlService;
+
+    @Post("/test-me")
+    @Consumes("application/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public FileCustomizableResponseType extractXML(@Body byte[] xml) throws XMLStreamException {
+        InputStream inpuStream = xmlService.createSumthing(xml);
+        return new StreamedFile(inpuStream, MediaType.APPLICATION_XML_TYPE).attach("data.xml");
+    }
+}
---
+++ src/main/java/net/krupizde/XmlService.java
@@ -0,0 +1,32 @@
+package net.krupizde;
+
+import javax.inject.Singleton;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@Singleton
+public class XmlService {
+
+    public InputStream createSumthing(byte[] xml) throws XMLStreamException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        XMLEventWriter writer = outputFactory.createXMLEventWriter(out, StandardCharsets.UTF_8.name());
+        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+        writer.add(eventFactory.createStartElement("","","root"));
+        for(int i = 0; i < 1000; i++){
+            XMLEventReader reader = xmlInputFactory.createXMLEventReader(new ByteArrayInputStream(xml), StandardCharsets.UTF_8.name());
+            writer.add(reader);
+            //System.err.println(out.toString());
+        }
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+}
